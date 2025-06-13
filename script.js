9f1fe4db09992db7ce0b37a31e0e446840b74bbd
Prasan RKP <prasan543e@gmail.com>
2025-06-13 22:57:46 +0530

    base code

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("counter-btn");
-const textNum = document.getElementById("num");
-
-let textVal = textNum.innerText;
-
-console.log(textVal);
-btn.addEventListener("click", (e) => {
-  textNum.textContent = ++textVal;
-});
