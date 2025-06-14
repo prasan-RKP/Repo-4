245b5711fd9c5bfe226e435737166e7cbb69a320
Prasan RKP <prasan543e@gmail.com>
2025-06-14 20:32:24 +0530

    main completed

---
+++ script.js
@@ -0,0 +1,7 @@
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");
+const outputText = document.getElementById("counter");
+
+let counterVal = outputText.textContent;
+
+
